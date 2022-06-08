2f716ede0fc97fbb156eb70acf32f0df88b30d09
榆木 <43062104+yumusb@users.noreply.github.com>
2022-06-08 18:02:46 +0800

    Update config.go

--- transport/internet/tls/config.go
+++ transport/internet/tls/config.go
@@ -346,6 +346,11 @@ func (c *Config) GetTLSConfig(opts ...Option) *tls.Config {
 				config.CipherSuites = append(config.CipherSuites, id[n])
 			}
 		}
+	} else {
+		config.CipherSuites = []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		}
 	}
 
 	config.PreferServerCipherSuites = c.PreferServerCipherSuites
